3301df2ba85843c2eba3b227f18da7bcca8d93e5
Anatoly Sorokin <lptolik@gmail.com>
2013-02-04 18:27:24 +0400

    fix dinucleotide calculation code to prevent missing or multiple dinucleotides.

--- R/dnaGeom.R
+++ R/dnaGeom.R
@@ -5,25 +5,33 @@ dnaGeom<-function(
   if(!require(seqinr)){
     stop('Required library "seqinr" is not installed.')
   }
-  s<-tolower(gsub(' ', '', s))
+  s<-tolower(gsub('\\s', '', s,perl=TRUE))
   l<-getLength(s)
   nseq<-s2n(s2c(s), levels=c("a", "t", "g", "c"), base4=FALSE)
-  
+  istarts <- seq(from = 1, to = l-1, by = 1)
+  oligos <- s2c(s)[istarts]
+  oligos <- paste(oligos, s2c(s)[istarts + 1], sep = "")
+
   #  load('rise_twist.Rdata')
   #	data(rise_twist,qqs,w)
   data<-rise_twist
-  
+  oligos.levels<-tolower(data$dilet)
+  din<-factor(oligos, levels = oligos.levels)
+
   di<-array(0, dim=c(16, l-1))
-  for (k in 1:16) {
-    i<-gregexpr(data$dilet[k], s, ignore.case=TRUE)
-    i<-as.vector(i[[1]])
-    di[k,i]<-array(1, dim=c(1, length(i)))
+#   for (k in 1:16) {
+#     i<-gregexpr(data$dilet[k], s, ignore.case=TRUE)
+#     i<-as.vector(i[[1]])
+#     di[k,i]<-array(1, dim=c(1, length(i)))
+#   }
+#   for (m in 1:(l-1)){ 
+#     if(sum(di[, m])!=1){ 
+#       di[, m]<-di[,m-1]
+#     }}
+  for(m in 1:l-1){
+    di[din[m],m]<-1
   }
-  for (m in 1:(l-1)){ 
-    if(sum(di[, m])!=1){ 
-      di[, m]<-di[,m-1]
-    }}
-  
+
   geom<-array(0, dim=c(2,l))
   phi<-cumsum(c(0,data$twist%*%di/180*pi))
   risem<-cumsum(c(0,data$rise%*%di))
